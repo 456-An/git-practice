74d9a1bddfc35f95ebd51eed89283bf62ac5a5a0
456 <asdwwooo52@gmail.com>
2026-07-04 16:27:46 +0800

    feat: add multiply and divide function

--- calculator.py
+++ calculator.py
@@ -4,6 +4,17 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Nan"
+    else:        
+        return a / b
+
 if __name__ == "__main__":
     print(f"3 + 5 = {add(3, 5)}")
     print(f"10 - 4 = {subtract(10, 4)}")
+    print(f"2 * 3 = {multiply(2, 3)}")
+    print(f"6 / 3 = {divide(6, 3)}")
